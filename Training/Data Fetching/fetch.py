e0f884317d429a6d9a776ea6602a88871aa5ebab
EetuPeltolaCodes <eddy.peltola@gmail.com>
2025-01-23 12:55:21 +0200

    Update fetch.py

--- Training/Data Fetching/fetch.py
+++ Training/Data Fetching/fetch.py
@@ -2,28 +2,50 @@ import requests
 
 response = requests.get('https://kyykka.com/api/teams/').json()
 
-for res in response:
-    id_response = requests.get(f'https://kyykka.com/api/teams/{res["id"]}/').json()
-    stats_2025 = id_response['2025']
-    mathc_played = stats_2025['match_count']
-    score_total = stats_2025['score_total']
-    throws_total = stats_2025['throws_total']
-    average_score = score_total / throws_total
-    pike_percentage = stats_2025['pike_percentage']
-    zeros_percentage = stats_2025['zero_percentage']
-    match_average = stats_2025['match_average']
-    players = stats_2025['players']
-    scaled_points_per_throw = []
-    for player in players:
-        if player['throws_total'] != 0:
-            scaled_points_per_throw.append(player['scaled_points_per_throw'])
-    average_scaled_points_per_throw = sum(scaled_points_per_throw) / len(scaled_points_per_throw)
-    print(f'Team: {stats_2025["current_abbreviation"]}')
-    print(f'Score total: {score_total}')
-    print(f'Pike percentage: {pike_percentage}')
-    print(f'Zeros percentage: {zeros_percentage}')
-    print(f'Match average: {match_average}')
-    print(f'Average scaled points per throw: {average_scaled_points_per_throw}')
-    print(f'Average score: {average_score}')
-    print('-----------------------------------')
-    
+with open('Training/Teams_data.csv', 'w') as file:
+    for res in response:
+        id_response = requests.get(f'https://kyykka.com/api/teams/{res["id"]}/').json()
+        stats_year = id_response['2025']
+        match_played = stats_year['match_count']
+        score_total = stats_year['score_total']
+        throws_total = stats_year['throws_total']
+        average_score = score_total / throws_total
+        pike_percentage = stats_year['pike_percentage']
+        zeros_percentage = stats_year['zero_percentage']
+        match_average = stats_year['match_average']
+        players = stats_year['players']
+        scaled_points_per_throw = []
+        for player in players:
+            if player['throws_total'] != 0:
+                scaled_points_per_throw.append(player['scaled_points_per_throw'])
+        average_scaled_points_per_throw = sum(scaled_points_per_throw) / len(scaled_points_per_throw)
+        print(stats_year['current_abbreviation'])
+        file.write(f"{res['id']},{match_played},{score_total},{throws_total},{average_score},{pike_percentage},{zeros_percentage},{match_average},{average_scaled_points_per_throw}\n")
+
+        
+        
+# Fetch more years than just 2025      
+'''
+        year = 2025
+        while True:
+            try:
+                stats_year = id_response[str(year)]
+                match_played = stats_year['match_count']
+                score_total = stats_year['score_total']
+                throws_total = stats_year['throws_total']
+                average_score = score_total / throws_total
+                pike_percentage = stats_year['pike_percentage']
+                zeros_percentage = stats_year['zero_percentage']
+                match_average = stats_year['match_average']
+                players = stats_year['players']
+                scaled_points_per_throw = []
+                for player in players:
+                    if player['throws_total'] != 0:
+                        scaled_points_per_throw.append(player['scaled_points_per_throw'])
+                average_scaled_points_per_throw = sum(scaled_points_per_throw) / len(scaled_points_per_throw)
+                print(stats_year['current_abbreviation'])
+                file.write(f"{res['id']},{match_played},{score_total},{throws_total},{average_score},{pike_percentage},{zeros_percentage},{match_average},{average_scaled_points_per_throw}\n")
+            except KeyError:
+                break
+            year -= 1
+'''
